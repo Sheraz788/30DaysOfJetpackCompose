a5112afef6943cba6ffb407d5cc8734f913562ac
Sheraz788 <sherazalibk@gmail.com>
2021-08-25 13:59:08 +0500

    Day 1 Complete

---
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.composelearning
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composelearning.ui.theme.ComposeLearningTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLearningTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Conversation(messages = SampleData.conversationSample)
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author : String, val body : String)
+
+@Composable
+fun Message(msg: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+       Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+           contentDescription = "Profile Picture",
+           modifier = Modifier
+               .size(40.dp)
+               .clip(CircleShape)
+               .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+       )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState (targetValue = if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Conversation(messages : List<Message>){
+    LazyColumn{
+        items(messages){ message ->
+            Message(message)
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
+
+//@Preview(showBackground = true)
+@Composable
+fun PreviewMessage() {
+    ComposeLearningTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
